03c55328bf9a0402ce6d26dbf8bc943d46d342be
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-08-22 08:33:20 +0300

    создание папки

---
+++ ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.Clear();
+
+string str1 = "Hello";
+string str2 = "World!";
+string str3=string.Concat(str1, str2, "####");
+//string str3=str1+ ", " + str2; //так добавить пробелы
+Console.WriteLine(str3);
+
